7f1c92a642a4e9370d4bd16eef729243dc66b041
Neto <netobjsc@gmail.com>
2022-03-21 23:36:51 -0300

    FundamentosJS

---
+++ type-number.js
@@ -0,0 +1,17 @@
+// tipo Number
+
+const meuNumero = 3;
+
+const num1 = 1;
+const num2 = 2;
+
+const operacaoMatematica = num1 + num2;
+
+const str1 = Number('1');
+const str2 = Number('12')
+
+console.log(num1, num2)
+console.log(operacaoMatematica)
+console.log(str1)
+console.log(str2)
+console.log(str1 + str2)
